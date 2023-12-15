ef1d81587c47eebe137833461580073500089c6f
NaN-jp <51327367+NaN-jp@users.noreply.github.com>
2023-12-15 23:20:52 +0900

    Add files via upload

---
+++ src/main/java/com/example/springbootpostgresql/model/Person.java
@@ -0,0 +1,23 @@
+package com.example.springbootpostgresql.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
+}
